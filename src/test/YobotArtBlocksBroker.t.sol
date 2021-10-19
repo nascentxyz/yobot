96c547380ce7fc0af3fc577fda29eff9fb4f8310
Andreas Bigger <abigger87@gmail.com>
2021-10-19 12:25:09 -0700

    broker

--- src/test/YobotArtBlocksBroker.t.sol
+++ src/test/YobotArtBlocksBroker.t.sol
@@ -5,11 +5,20 @@ import { DSTestPlus } from "./utils/DSTestPlus.sol";
 
 import { YobotArtBlocksBroker } from "../YobotArtBlocksBroker.sol";
 
-contract YobotArtBlocksBrokerTest is DSTest {
+
+contract YobotArtBlocksBrokerTest is DSTestPlus {
     YobotArtBlocksBroker yabb;
 
-    function setUp() public {
-        yabb = new YobotArtBlocksBroker();
+    /// @dev internal contract state
+    address public coordinator;
+	address public profitReceiver;
+	uint256 public artBlocksBrokerFeeBips;
+
+    function setUp(address _coordinator, address _profitReceiver, uint256 _artBlocksBrokerFeeBips) public {
+        coordinator = _coordinator;
+        profitReceiver = _profitReceiver;
+        artBlocksBrokerFeeBips = _artBlocksBrokerFeeBips;
+        yabb = new YobotArtBlocksBroker(profitReceiver, artBlocksBrokerFeeBips);
     }
 
     function testFail_basic_sanity() public {
