7c277791d9f6f91d706e4344b9200a4762d00dc5
Andreas Bigger <abigger87@gmail.com>
2022-03-02 12:36:07 -0800

    :gear: marquee fix

--- src/components/AlphaMarquee.tsx
+++ src/components/AlphaMarquee.tsx
@@ -8,9 +8,19 @@ const AlphaMarquee = ({ isAlpha = true }) => {
           <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
           <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
           <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
         </div>
 
         <div className="absolute top-0 py-4 animate-marquee2 whitespace-nowrap bg-black text-white font-Rubik text-sm">
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
+          <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
           <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
           <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
           <span className="mx-4">YOBOT IS IN ALPHA — USE AT YOUR OWN RISK</span>
